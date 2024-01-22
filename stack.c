ffd9837e3a43b20d1dcd775ba1fbd0d2d710b03b
stephen barracks asemiah <barrackobarracko1@gmail.com>
2024-01-21 20:47:57 -0800

    Stacks, Queues - LIFO, FIFO

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - this prints the top
+ * @head: the stack head
+ * @counter: the line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
